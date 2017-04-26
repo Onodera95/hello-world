907b0ccdc8c53e48ac8beffd33fc528fab4b1c35
Onodera95 <uchkalec1995@mail.ru>
2017-04-26 11:24:15 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <h3>Должность</h3>
+    </div>
+    <div class="row">
+        <table class="table table-striped table-bordered">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Наименование</th>                
+            </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</div> <!-- /container -->
+</body>
+</html>
