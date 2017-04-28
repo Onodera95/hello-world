88b65be22fdd7b9d316866d95aea49b1a96c567a
Uchkalec Olga <uchkalec1995@mail.ru>
2017-04-28 11:24:51 +0300

    Добавлены таблицы Дополнительная информация, статус

---
+++ dop_inf.php
@@ -0,0 +1,56 @@
+<html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Дополнительная информация</title>
+    <style type="text/css">
+        TABLE {
+            width: 300px; /* Ширина таблицы */
+            border-collapse: collapse; /* Убираем двойные линии между ячейками */
+        }
+        TH {
+            padding: 10px; /* Поля вокруг содержимого таблицы */
+            border: 1px solid black; /* Параметры рамки */
+        }
+        TD {
+            /*background: #b0e0e6; /* Цвет фона */
+            padding: 5px; /* Поля вокруг содержимого таблицы */
+            border: 1px solid black; /* Параметры рамки */
+            text-align: center; /* Выравнивание текста */
+        }
+
+    </style>
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <h3>Дополнительная информация</h3>
+    </div>
+    <div class="row">
+        <table class="table table-striped table-bordered">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Инструктах</th>
+                <th>Дата проведения</th>
+                <th>Должность</th>
+                <th>Статус</th>
+            </tr>
+            </thead>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <!-- /присоединить к БД/ -->
+            </tr>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</div> <!-- /container -->
+</body>
+</html>
---
+++ instrukt.php
@@ -0,0 +1,53 @@
+<html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Инструктаж</title>
+    <style type="text/css">
+        TABLE {
+            width: 300px; /* Ширина таблицы */
+            border-collapse: collapse; /* Убираем двойные линии между ячейками */
+        }
+        TH {
+            padding: 10px; /* Поля вокруг содержимого таблицы */
+            border: 1px solid black; /* Параметры рамки */
+        }
+        TD {
+            /*background: #b0e0e6; /* Цвет фона */
+            padding: 5px; /* Поля вокруг содержимого таблицы */
+            border: 1px solid black; /* Параметры рамки */
+            text-align: center; /* Выравнивание текста по левому краю */
+        }
+    </style>
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <h3>Инструктаж</h3>
+    </div>
+    <div class="row">
+        <table class="table table-striped table-bordered">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Наименование</th>
+                <th>Описание</th>
+                <th>Дополнительная иниформация</th>
+            </tr>
+            </thead>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <!-- /присоединить к БД/ -->
+            </tr>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</div> <!-- /container -->
+</body>
+</html>
